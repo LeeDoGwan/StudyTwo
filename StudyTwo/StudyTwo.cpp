d72d124f69bf6c49623fe4faf4ad9adf5c489da2
LeeDoGwan <56504630+LeeDoGwan@users.noreply.github.com>
2020-06-09 10:00:29 +0900

    초기 커밋.

---
+++ StudyTwo/StudyTwo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void main(void) {
+	int a = 7;
+	float b = 8.0;
+	char c = 'K';
+	double d = 10.0;
+	string s = "Hello";
+	char buff[] = "World";
+
+}
+
